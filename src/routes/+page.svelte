bfd8577deb1ee03fb02f4e4c68ddc857a63e5d9c
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-12-29 20:31:37 +0900

    migration to SvelteKit 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,15 @@
     currentName,
     stringOfNames,
   } from '$lib/stores.js';
-  import ProjectTitle from './components/ProjectTitle.svelte';
-  import NamesTextArea from './components/NamesTextArea.svelte';
-  import SetNamesButton from './components/SetNamesButton.svelte';
-  import ClearNamesButton from './components/ClearNamesButton.svelte';
-  import RandomNameButton from './components/RandomNameButton.svelte';
-  import CurrentName from './components/CurrentName.svelte';
-  import ListOfNames from './components/ListOfNames.svelte';
-  import ExhaustedList from './components/ExhaustedList.svelte';
-  import ListProgress from './components/ListProgress.svelte';
+  import ProjectTitle from '$components/ProjectTitle.svelte';
+  import NamesTextArea from '$components/NamesTextArea.svelte';
+  import SetNamesButton from '$components/SetNamesButton.svelte';
+  import ClearNamesButton from '$components/ClearNamesButton.svelte';
+  import RandomNameButton from '$components/RandomNameButton.svelte';
+  import CurrentName from '$components/CurrentName.svelte';
+  import ListOfNames from '$components/ListOfNames.svelte';
+  import ExhaustedList from '$components/ExhaustedList.svelte';
+  import ListProgress from '$components/ListProgress.svelte';
 </script>
 
 <div class="page-container">
